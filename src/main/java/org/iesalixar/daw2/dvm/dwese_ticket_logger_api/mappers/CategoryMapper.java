7d44ec3fd232e994dc7f4e3cf1691640d3b6b845
eldaniii <dvilmar309@g.educaand.es>
2025-01-10 11:13:32 +0100

    Actualizando category 2

--- src/main/java/org/iesalixar/daw2/dvm/dwese_ticket_logger_api/mappers/CategoryMapper.java
+++ src/main/java/org/iesalixar/daw2/dvm/dwese_ticket_logger_api/mappers/CategoryMapper.java
@@ -1,4 +1,30 @@
 package org.iesalixar.daw2.dvm.dwese_ticket_logger_api.mappers;
 
+import org.iesalixar.daw2.dvm.dwese_ticket_logger_api.dtos.CategoryCreateDTO;
+import org.iesalixar.daw2.dvm.dwese_ticket_logger_api.dtos.CategoryDTO;
+import org.iesalixar.daw2.dvm.dwese_ticket_logger_api.entities.Category;
+
 public class CategoryMapper {
+
+    public CategoryDTO toDTO(Category category) {
+        CategoryDTO dto = new CategoryDTO();
+        dto.setId(category.getId());
+        dto.setCode(category.getCode());
+        dto.setName(category.getName());
+        return dto;
+    }
+
+    public Category toEntity(CategoryDTO dto) {
+        Category category = new Category();
+        category.setCode(dto.getCode());
+        category.setName(dto.getName());
+        return category;
+    }
+
+    public Category toEntity(CategoryCreateDTO createDTO) {
+        Category category = new Category();
+        category.setCode(createDTO.getCode());
+        category.setName(createDTO.getName());
+        return category;
+    }
 }
